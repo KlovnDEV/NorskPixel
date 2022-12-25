8ef6b3c75a24175b9b1607ad8cbb68b1f6a0e11a
RainoDEV2 <113799393+RainoDEV2@users.noreply.github.com>
2022-12-25 12:20:03 +0100

    oversetting av norskpixel-radio

--- resources/[voice]/norskpixel-radio/config.lua
+++ resources/[voice]/norskpixel-radio/config.lua
@@ -17,15 +17,15 @@ Config.RestrictedChannels = {
 Config.MaxFrequency = 500
 
 Config.messages = {
-  ['not_on_radio'] = 'Du er ikke forbundet til en kanal',
-  ['on_radio'] = 'Du er allerede forbundet til kanalen',
-  ['joined_to_radio'] = 'Du er forbundet til: ',
-  ['restricted_channel_error'] = 'Du kan ikke forbindes til frekvensen!',
+  ['not_on_radio'] = 'Du er ikke koblet til en kanal',
+  ['on_radio'] = 'Du er allerede koblet til kanalen',
+  ['joined_to_radio'] = 'Du er koblet til: ',
+  ['restricted_channel_error'] = 'Du kan ikke kobles til frekvensen!',
   ['invalid_radio'] = 'Denne frekvens er ikke ledig',
-  ['you_on_radio'] = 'Du er allerede forbundet til kanalen',
+  ['you_on_radio'] = 'Du er allerede koblet til kanalen',
   ['you_leave'] = 'Du forlod kanalen.',
   ['volume_radio'] = 'Ny lydstyrke ',
-  ['decrease_radio_volume'] = 'Radio lydstyrken er allerede sat på maks',
-  ['increase_radio_volume'] = 'Radio lydstryken kan ikke være lavere',
+  ['decrease_radio_volume'] = 'Radio lydstyrken er allerede satt på maks',
+  ['increase_radio_volume'] = 'Radio lydstyrken kan ikke være lavere',
   ['increase_decrease_radio_channel'] = 'Ny kanal ',
 }
