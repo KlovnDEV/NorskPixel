70a84c5cfa1f5513b52892a7802f4bd50b0c589f
RainoDEV2 <113799393+RainoDEV2@users.noreply.github.com>
2022-12-25 12:57:08 +0100

    adminmeny

---
+++ resources/[qb]/mercy-adminmenu_qb/fxmanifest.lua
@@ -0,0 +1,53 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mercy Collective (https://dsc.gg/mercy-coll)'
+description 'Admin Menu'
+
+ui_page "nui/index.html"
+
+shared_scripts {
+    'shared/sh_config.lua',
+    'locale.lua',
+    'locales/en.lua', -- Change this to your desired language.
+}
+
+client_scripts {
+    '@norskpixel-core/shared/items.lua',
+    '@norskpixel-core/shared/gangs.lua',
+    '@norskpixel-core/shared/jobs.lua',
+    '@norskpixel-core/shared/vehicles.lua',
+    'client/**/cl_*.lua',
+    'shared/sh_commands.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/**/sv_*.lua',
+}
+
+files {
+    "nui/index.html",
+    "nui/js/**.js",
+    "nui/css/**.css",
+    "nui/webfonts/*.css",
+    "nui/webfonts/*.otf",
+    "nui/webfonts/*.ttf",
+    "nui/webfonts/*.woff2",
+}
+
+exports {
+    'CreateLog',
+    'ToggleDev',
+}
+
+server_exports {
+    'CreateLog'
+} 
+
+dependencies {
+    'oxmysql',
+    'norskpixel-core'
+}
+
+lua54 'yes'
